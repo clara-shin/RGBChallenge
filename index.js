a81b963db3dd25b22f48602fe5a971c1ec865884
Hyejin Shin <enthusiastic.shj@gmail.com>
2017-12-21 17:07:07 +0900

    rgb챌린지 구현

--- index.js
+++ index.js
@@ -4,10 +4,49 @@ function random255() {
 
 
 function randomColor() {
-  return `rgb(${random255()},${random255()},${random255()})`;
+  return `rgb(${random255()}, ${random255()}, ${random255()})`;
+}
+
+let stage;
+let problem;
+let correctAnswer;
+function nextStage() {
+  //다음 단계에 대한 상태로 전환
+  stage++;
+  problem = [randomColor(), randomColor(), randomColor()];
+}
+
+function draw() {
+  //화면그리기
+  document.querySelectorAll('.box').forEach((el, index) => {
+    el.style.backgroundColor = problem[index];
+  });
+  document.querySelector('.rgb-text').textContent = problem[correctAnswer];
+  document.querySelector('.score').textContent= `SCORE: ${stage}`;
+}
+//초기화
+function init() {
+  stage = 0;
+  problem = [randomColor(), randomColor(), randomColor()];
+  correctAnswer = Math.floor(Math.random() * 3); //0 1 2
 }
 
 
-document.querySelectorAll('.box').forEach(el => {
-  el.style.backgroundColor = randomColor();
+init();
+
+document.querySelectorAll('.box').forEach((el, index) => {
+  el.addEventListener('click', e => {
+    if (index === correctAnswer) {
+      nextStage();
+      draw();
+    }else{
+      init();
+      draw();
+    }
+  });
 });
+
+draw();
+
+//상태를 저장할 저장소 먼저 만들고
+//어떻게 보여질것인지 생각
